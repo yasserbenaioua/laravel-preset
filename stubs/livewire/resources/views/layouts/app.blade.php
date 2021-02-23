aa52992a29d65a639b2ef446a2604bc412859c5e
Yasser Benaioua <yasse.bn1997@gmail.com>
2021-02-23 18:54:34 +0100

    wip

--- stubs/livewire/resources/views/layouts/app.blade.php
+++ stubs/livewire/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" rtl="ltr">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
